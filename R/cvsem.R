62c8054a507b3edef4dea35bb37eb4070d3fd06f
Anna Wysocki <awysocki@ucdavis.edu>
2022-04-22 12:00:14 -0700

    Added some placeholder help information to the cvsem function

--- R/cvsem.R
+++ R/cvsem.R
@@ -1,3 +1,21 @@
+
+
+
+#' Do model comparison on SEM models using cross-validation
+#'
+#' @param x Data
+#' @param Models A list of specified models in lavaan syntax
+#' @param k The number of folds. Default is 5.
+#'
+#' @return A list with the prediction error for each model.
+#' @export
+#'
+#' @examples
+#' cvsem(data, list(x, y, z), k = 5)
+
 cvsem <- function(){
   print("Place holder for cv function")
 }
+
+
+
