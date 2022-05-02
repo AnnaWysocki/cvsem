c0a88f11dbebcc00551ee7c0464f45234035c121
Anna Wysocki <awysocki@ucdavis.edu>
2022-05-02 15:51:50 -0700

    Commit with error-- need to fix

---
+++ R/createFolds.R
@@ -0,0 +1,35 @@
+createFolds <- function(data, k = 5){
+
+  s <- nrow(data)/k
+
+  folds <- list(NULL)
+
+
+
+  if( all.equal(s, as.integer(s)) != TRUE ){
+    s <- floor(s)
+  }
+
+  for( i in 0:(k-1) ) {
+
+    rowstart <- 1 + (i * s)
+
+    if( (i + 1) < k) {
+
+      rowstop <- (i + 1) * s
+
+    }else{
+
+      rowstop <- nrow(data)
+    }
+
+    subsetrows <- rowstart:rowstop
+
+    test <- data[subsetrows, ]
+    training <- data[-subsetrows,]
+
+    folds[[i + 1]] <- list(test = test, training = training)
+
+  }
+  return(folds)
+}
---
+++ R/lavaanWrapper.R
@@ -0,0 +1,16 @@
+lavaanWrapper <- function( data, Models ){
+
+  if( is.list(Models) == FALSE ){
+    stop( "Input for the Models arugment is not a list" )
+  }
+
+  ModelNumber <- length(Models)
+
+  for( i in 1: length(ModelNumber) ){
+    if( class(Models[[i]]) !=  "lavaan" ){
+      stop( paste("Model", i, "is not a lavaan object") )
+    }
+  }
+
+  return("lavaan model matrix")
+}
