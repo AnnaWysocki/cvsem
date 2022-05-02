a2f815565c733b38ae5e490655c612b0d2551c4e
ph-rast <rast.ph@gmail.com>
2022-05-02 16:11:26 -0700

    test folder

---
+++ tests/createfolds.R
@@ -0,0 +1,27 @@
+##' .. content for \description{} (no empty lines) ..
+##'
+##' .. content for \details{} ..
+##' @title 
+##' @param data 
+##' @param K 
+##' @param ... 
+##' @return 
+##' @author philippe
+cv <- function(data, K = 10, ...) {
+  cv_dat <- caret::createFolds(y =  data, k =  K)
+  return( cv_dat )
+}
+
+
+data <-  data.frame(replicate(5, rnorm(100)))
+data
+
+
+testIndices <- caret::createFolds(y =  data)
+num_Folds <- length(testIndices )
+
+test <- data[testIndices[[1]]]
+train <- data[-testIndices[[1]]]
+train
+
+
