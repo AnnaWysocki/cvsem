af28d2f98db74c10a096d37e3dd4b402dd047769
Anna Wysocki <awysocki@ucdavis.edu>
2022-05-23 15:07:31 -0700

    added draft frunction for KL-divergence

--- R/cvsem.R
+++ R/cvsem.R
@@ -3,6 +3,7 @@
 #' @param x Data
 #' @param Models A list of specified models in lavaan syntax
 #' @param k The number of folds. Default is 5.
+#' @param lavaan_function Specify which lavaan function to use. Default is "sem". Other options are "lavaan" and "cfa"
 #'
 #' @return A list with the prediction error for each model.
 #' @export
@@ -27,13 +28,19 @@
 #'
 
 
-cvsem <- function(x, Models, k = 5){
+cvsem <- function(x, Models, k = 5, lavaan_function = "sem"){
+
+  stopifnot("`k` must be numeric " = is.numeric(k))
+
+  match.arg(arg = lavaan_function, choices = c("sem", "lavaan", "cfa"))
 
   model_number <- length(Models)
   model_cv <- data.frame(Model = rep(0, model_number),
                          Cross_Validation = rep(0, model_number))
 
   folds <- createFolds(x, k = k)
+
+  if( nrow(folds[[1]]$test) < 2 ) stop('Covariance matrix cannot be computed because at least one of your test folds has fewer than two rows of data.')
 
   if(is.null(names(Models)) != TRUE){
 
@@ -51,7 +58,14 @@ cvsem <- function(x, Models, k = 5){
     for(i in 1:k){
 
       train_data <- folds[[i]]$training
-      model_results <- lavaan::sem(model = model, data = train_data)
+
+      if(lavaan_function == "sem"){
+        model_results <- lavaan::sem(model = model, data = train_data)
+      } else if (lavaan_function == "lavaan"){
+        model_results <- lavaan::lavaan(model = model, data = train_data)
+      } else{
+        model_results <- lavaan::cfa(model = model, data = train_data)}
+
 
       implied_sigma_lavaan <- lavaan::inspect(model_results, what = "cov.ov")
 
@@ -62,9 +76,16 @@ cvsem <- function(x, Models, k = 5){
 
       test_S <- stats::cov(folds[[i]]$test[, colnames(lavaan::inspect(model_results, what = "data"))])
 
+      if( any(is.na(test_S)) == TRUE ) stop('NAs have been returned for a test covariance matrix.
+                                            Try decreasing the number of folds.')
+
       ## Maximum Wishart Likelihood (MWL), Cudeck & Browne (1983) CV covariance structures
       ## Note, C&B use det(S)=|S|
-      distance <- log( det(implied_sigma) ) - log( det(test_S) ) + sum(diag( test_S %*% solve(implied_sigma) ) ) - ncol(test_S)      
+      distance <- log( det(implied_sigma) ) - log( det(test_S) ) + sum(diag( test_S %*% solve(implied_sigma) ) ) - ncol(test_S)
+
+      if(is.na(distance) == TRUE){
+        stop("Cross validation index cannot be computed. Try decreasing the number of folds.")
+      }
 
       ## collect distance metrics for each fold
       cv_index[i] <- distance
